55683155c52ea00595d818d3defbd9e5e1ec312e
Guilherme Sabbado <guilherme.sabbado@indicium.tech>
2025-10-10 02:38:54 +0000

    Finish source tests. Starting int layer

---
+++ models/marts/core/intermediate/int_customer.sql
@@ -0,0 +1,31 @@
+with customer as (
+    select * from {{ ref('stg_customer')}}
+),
+
+person as (
+    select * from {{ ref('stg_person')}}
+),
+
+store as (
+    select * from {{ ref('stg_store')}}
+)
+
+, joined as (
+    select
+        customer.customer_id,
+        customer.person_id,
+        customer.store_id,
+        customer.territory_id,
+        person.business_entity_id as person_business_entity_id,
+        person.person_type as person_type,
+        person.first_name,
+        person.middle_name,
+        person.full_name,
+        store.business_entity_id as store_business_entity_id,
+        store.store_name
+        from customer
+        left join person on customer.person_id = person.business_entity_id
+        left join store on customer.store_id = store.business_entity_id        
+        
+)
+select * from joined
---
+++ models/marts/core/intermediate/int_products.sql
@@ -0,0 +1,31 @@
+with product as (
+    select * from {{ ref('stg_product')}}
+),
+
+category as (
+    select * from {{ ref('stg_product_category')}}
+),
+
+subcategory as (
+    select * from {{ ref('stg_product_subcategory')}}
+)
+
+, joined as (
+    select
+        product.product_id,
+        product.product_name,
+        product.product_name,
+        product.product_subcategory_id,
+        product.standard_cost,
+        product.list_price,
+        category.product_category_id,
+        category.product_category_name as category,
+        subcategory.product_subcategory_id, 
+        subcategory.product_category_id,
+        subcategory.product_category_name,
+        from product
+        left join subcategory on product.product_subcategory_id = subcategory.product_subcategory_id
+        left join category on subcategory.product_category_id = category.product_category_id        
+        
+)
+select * from joined
